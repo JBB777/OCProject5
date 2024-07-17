f9a4653e3ac748c44b574d372b12ba4da2a04974
JBB <jbaptistebesancon@gmail.com>
2024-07-17 12:06:32 +0200

    Etapes 1-2-3 réalisées

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,27 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let arrowLeft = document.querySelector(".arrow_left");
+	arrowLeft.addEventListener("click", (event) => {
+	console.log("Vous avez cliqué sur la flèche gauche")
+});
+
+let arrowRight = document.querySelector(".arrow_right");
+	arrowRight.addEventListener("click", (event) => {
+	console.log("Vous avez cliqué sur la flèche droite")
+});
+
+
+/*let dotsNumber = slides.length;
+console.log(dotsNumber)*/
+
+for (let i = 0; i < slides.length; i++) {
+	let dot = document.createElement("div")
+	dot.classList.add("dot")
+	let parentElement = document.querySelector(".dots")
+	parentElement.appendChild(dot)
+	if (i == 0 ) {
+		dot.classList.add("dot_selected")
+	}
+}
