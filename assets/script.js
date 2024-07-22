0aac5c4be14f2472af917b8172393aa2d488deca
JBB <jbaptistebesancon@gmail.com>
2024-07-22 09:10:13 +0200

    Modification du dot selectionné au changement d'image via un clic

--- assets/script.js
+++ assets/script.js
@@ -18,22 +18,22 @@ const slides = [
 ]
 
 
-let divParentBanner = document.getElementById("banner")
+// let divParentBanner = document.getElementById("banner")
 
 /* Affichage des deux flèches gauche et droite du carousel*/
-let imgArrowLeft = document.createElement("img")
-	imgArrowLeft.src = './assets/images/arrow_left.png'
-	imgArrowLeft.alt = 'Arrow Left'
-	imgArrowLeft.classList.add("arrow")
-	imgArrowLeft.classList.add("arrow_left")
-	divParentBanner.appendChild(imgArrowLeft)
+// let imgArrowLeft = document.createElement("img")
+// 	imgArrowLeft.src = './assets/images/arrow_left.png'
+// 	imgArrowLeft.alt = 'Arrow Left'
+// 	imgArrowLeft.classList.add("arrow")
+// 	imgArrowLeft.classList.add("arrow_left")
+// 	divParentBanner.appendChild(imgArrowLeft)
 
-let imgArrowRight = document.createElement("img")
-	imgArrowRight.src = './assets/images/arrow_right.png'
-	imgArrowRight.alt = 'Arrow Right'
-	imgArrowRight.classList.add("arrow")
-	imgArrowRight.classList.add("arrow_right")
-	divParentBanner.appendChild(imgArrowRight)
+// let imgArrowRight = document.createElement("img")
+// 	imgArrowRight.src = './assets/images/arrow_right.png'
+// 	imgArrowRight.alt = 'Arrow Right'
+// 	imgArrowRight.classList.add("arrow")
+// 	imgArrowRight.classList.add("arrow_right")
+// 	divParentBanner.appendChild(imgArrowRight)
 
 
 
@@ -54,14 +54,52 @@ for (let i = 0; i < slides.length; i++) {
 
 
 let text = document.querySelector("#banner p")
+let tabDots = document.querySelectorAll(".dot")
+
+// console.log(tabDots.length)
+
+
+
+/* Fonction affichage du dot suivant ou précédent au clic */
+function dotSuivant(tabDots, i) {
+	if (tabDots[i].className == 'dot dot_selected') {
+		tabDots[i].classList.remove("dot_selected")
+		if (i == tabDots.length-1) {
+			tabDots[0].classList.add('dot_selected')
+		}
+		else {
+			tabDots[i+1].classList.add('dot_selected');
+		}
+	}
+}
+
+function dotPrecedent(tabDots, i) {
+	if (tabDots[i].className == 'dot dot_selected') {
+		tabDots[i].classList.remove("dot_selected")
+		if (i == 0) {
+			tabDots[tabDots.length-1].classList.add('dot_selected')
+		}
+		else {
+			tabDots[i-1].classList.add('dot_selected');
+		}
+	}
+}
+
+
+// for (let i = 0; i<tabDots.length; i++){
+// 	console.log(tabDots[i].className)
+// };
+
 
 /* Gestion des clic sur les flèches */
+/* Flèche gauche */
 let arrowLeft = document.querySelector(".arrow_left");
 	arrowLeft.addEventListener("click", (event) => {
 	let imgAffichee = document.querySelector(".banner-img")
 	let imgName = imgAffichee.src.substring(imgAffichee.src.lastIndexOf("/")+1, imgAffichee.src.length)
 
 	for (let i = 0; i < slides.length; i++) {
+		dotPrecedent(tabDots, i)
 		if (imgName == slides[i].image) {
 			if (i == 0) {
 				imgAffichee.src = './assets/images/slideshow/' + slides[slides.length-1].image
@@ -78,12 +116,14 @@ let arrowLeft = document.querySelector(".arrow_left");
 	
 });
 
+/* Flèche droite */
 let arrowRight = document.querySelector(".arrow_right");
 	arrowRight.addEventListener("click", (event) => {
 	let imgAffichee = document.querySelector(".banner-img")
 	let imgName = imgAffichee.src.substring(imgAffichee.src.lastIndexOf("/")+1, imgAffichee.src.length)
 
 	for (let i = 0; i < slides.length; i++) {
+		dotSuivant(tabDots, i)
 		if (imgName == slides[i].image) {
 			if (i == slides.length-1) {
 				imgAffichee.src = './assets/images/slideshow/' + slides[0].image
