af61a236ca845cd5e87f25bff8fbe87fc5245417
JBB <jbaptistebesancon@gmail.com>
2024-07-24 12:44:56 +0200

    Réécriture fonction slide et method au clic + mis en page du JS

--- assets/script.js
+++ assets/script.js
@@ -17,124 +17,69 @@ const slides = [
 	}
 ]
 
-
-// let divParentBanner = document.getElementById("banner")
-
-/* Affichage des deux flèches gauche et droite du carousel*/
-// let imgArrowLeft = document.createElement("img")
-// 	imgArrowLeft.src = './assets/images/arrow_left.png'
-// 	imgArrowLeft.alt = 'Arrow Left'
-// 	imgArrowLeft.classList.add("arrow")
-// 	imgArrowLeft.classList.add("arrow_left")
-// 	divParentBanner.appendChild(imgArrowLeft)
-
-// let imgArrowRight = document.createElement("img")
-// 	imgArrowRight.src = './assets/images/arrow_right.png'
-// 	imgArrowRight.alt = 'Arrow Right'
-// 	imgArrowRight.classList.add("arrow")
-// 	imgArrowRight.classList.add("arrow_right")
-// 	divParentBanner.appendChild(imgArrowRight)
-
-
-
-let parentElement = document.querySelector(".dots")
-
-/* Boucle permettant l'affichage des dots */
-for (let i = 0; i < slides.length; i++) {
-	let dot = document.createElement("div")
-	dot.classList.add("dot")
-	parentElement.appendChild(dot)
-
-	/* Définition du dot n°1 qui sera selected */
-	if (i == 0 ) {
-		dot.classList.add("dot_selected")
-	}
-}
-	
-
-
-let text = document.querySelector("#banner p")
-let tabDots = document.querySelectorAll(".dot")
-
-// console.log(tabDots.length)
-
-
-
-/* Fonction affichage du dot suivant ou précédent au clic */
-function dotSuivant(tabDots, i) {
-	if (tabDots[i].className == 'dot dot_selected') {
-		tabDots[i].classList.remove("dot_selected")
-		if (i == tabDots.length-1) {
-			tabDots[0].classList.add('dot_selected')
-		}
-		else {
-			tabDots[i+1].classList.add('dot_selected');
-		}
-	}
-}
-
-function dotPrecedent(tabDots, i) {
-	if (tabDots[i].className == 'dot dot_selected') {
-		tabDots[i].classList.remove("dot_selected")
-		if (i == 0) {
-			tabDots[tabDots.length-1].classList.add('dot_selected')
-		}
-		else {
-			tabDots[i-1].classList.add('dot_selected');
-		}
-	}
-}
-
-
-// for (let i = 0; i<tabDots.length; i++){
-// 	console.log(tabDots[i].className)
-// };
-
-
-/* Gestion des clic sur les flèches */
-/* Flèche gauche */
+/*************/
+/* VARIABLES */
+/*************/
+const parentElement = document.querySelector(".dots");
+let text = document.querySelector("#banner p");
 let arrowLeft = document.querySelector(".arrow_left");
-	arrowLeft.addEventListener("click", (event) => {
-	let imgAffichee = document.querySelector(".banner-img")
-	let imgName = imgAffichee.src.substring(imgAffichee.src.lastIndexOf("/")+1, imgAffichee.src.length)
+let arrowRight = document.querySelector(".arrow_right");
+let index = 0;
+let imgOnScreen = document.querySelector(".banner-img");
 
-	for (let i = 0; i < slides.length; i++) {
-		dotPrecedent(tabDots, i)
-		if (imgName == slides[i].image) {
-			if (i == 0) {
-				imgAffichee.src = './assets/images/slideshow/' + slides[slides.length-1].image
-				text.innerHTML = slides[slides.length-1].tagLine
-				break
-			}
-			else {
-				imgAffichee.src = './assets/images/slideshow/' + slides[i-1].image
-				text.innerHTML = slides[i-1].tagLine
-				break
-			}
-		}
+
+/*************/
+/* FUNCTIONS */
+/*************/
+
+/* Loop display dots */
+for (index = 0; index < slides.length; index++) {
+	let dot = document.createElement("div");
+	dot.classList.add("dot");
+	parentElement.appendChild(dot);
+};
+index = 0; // Reset index to 0
+
+parentElement.firstChild.classList.add("dot_selected"); // First dot is selected
+
+let tabDots = document.querySelectorAll(".dot");
+
+/* Slide function */
+function doSlice (index) {
+	tabDots[index].classList.add('dot_selected');
+	imgOnScreen.src = './assets/images/slideshow/' + slides[index].image;
+	text.innerHTML = slides[index].tagLine;
+}
+
+
+/**********/
+/* METHOD */
+/**********/
+
+/* Click on arrow */
+/* Left arrow */
+arrowLeft.addEventListener("click", () => {
+	tabDots[index].classList.remove("dot_selected");
+	if (index > 0) {
+		doSlice(index-1);
+		index-=1;
+	}
+	else {
+		doSlice(tabDots.length-1);
+		index = tabDots.length-1;
 	}
 	
 });
 
-/* Flèche droite */
-let arrowRight = document.querySelector(".arrow_right");
-	arrowRight.addEventListener("click", (event) => {
-	let imgAffichee = document.querySelector(".banner-img")
-	let imgName = imgAffichee.src.substring(imgAffichee.src.lastIndexOf("/")+1, imgAffichee.src.length)
-
-	for (let i = 0; i < slides.length; i++) {
-		dotSuivant(tabDots, i)
-		if (imgName == slides[i].image) {
-			if (i == slides.length-1) {
-				imgAffichee.src = './assets/images/slideshow/' + slides[0].image
-				text.innerHTML = slides[0].tagLine
-				break
-			}
-			else {
-				imgAffichee.src = './assets/images/slideshow/' + slides[i+1].image
-				text.innerHTML = slides[i+1].tagLine
-				break
-			}
-		}
+/* Right arrow */
+arrowRight.addEventListener("click", () => {
+	tabDots[index].classList.remove("dot_selected");
+	if (index < tabDots.length-1) {
+		doSlice(index+1);
+		index+=1;
+	}
+	else {
+		doSlice(0);
+		index = 0;
 	}
 });
